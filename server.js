03635267eb83521dbceacd4421356cafe945d3e0
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-06-14 17:59:45 -0500

    added the homepage

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,3 +17,9 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
